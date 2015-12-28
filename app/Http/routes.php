da509e22397a7784b3f3742d49a3887ad578ad4e
Nishanth <nishanthkd02@gmail.com>
2015-12-28 19:33:20 +0530

    framework upgrade from 5.1.* to 5.2.*

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,10 +28,35 @@ Route::get('/start', 'TestroleController@start');
 Route::get('/show', 'TestroleController@show');
 
 
-Route:: controllers([
+/*
+
+-> Deprecated in 5.2 => Route::controllers();
+-> Replaced with explicit route registration
+
+Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+*/
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('home/removereferral', 'HomeController@removeReferral');
