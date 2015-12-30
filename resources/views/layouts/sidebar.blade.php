8ba787ff75bf66b2c477b0433a3785471ab934a4
bohraji <tusharbohra01@gmail.com>
2015-12-30 12:36:24 +0530

    New staic sidebar updates according to current theme

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -54,6 +54,9 @@
 
 -->
 <!--sidebar begin-->
+
+
+<!--
 <div class="section_1 center">
     <h3 class="title"><span><img src="{{URL::asset('images/sidebar/care_coordination_small.png')}}" class="image"></span>Care Coordination</h3>
 </div>
@@ -75,6 +78,44 @@
     </div>
 </div>
 </div>
+-->
+
+<div class="container no-padding">
+<div class="row section_1 center">
+<div class="col-lg-2 col-md-2">
+<div class="dropdown" >
+    <button class="dropdown-toggle admin_button" type="button" data-toggle="dropdown" ><img src="{{URL::asset('images/sidebar/care_coordination_small.png')}}">
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu" >
+      <li class="hello"><a href="#"><img src="{{URL::asset('images/sidebar/messages.png')}}" class="drop_image"></a></li>
+      <li><a href="#"><img src="{{URL::asset('images/sidebar/file_update.png')}}" class="drop_image"></a></li>
+      <li><a href="#"><img src="{{URL::asset('images/sidebar/announcements.png')}}" class="drop_image"></a></li>
+      <li><a href="#"><img src="{{URL::asset('images/sidebar/schedule.png')}}" class="drop_image"></a></li>
+      <li><a href="#"><img src="{{URL::asset('images/sidebar/records.png')}}" class="drop_image"></a></li>
+      <li><a href="#"><img src="{{URL::asset('images/sidebar/care_coordination.png')}}" class="drop_image"></a></li>
+    </ul>
+    </div>
+    </div>
+    <div class="col-lg-9 col-md-10"> <h3 class="title">Administration</h3></div>
+</div>
+    <div class="row admin">
+        <a class="sidebar-item" href="#">
+            <div class="col-lg-6 col-md-6 patients font_change">Patients</div><div class="col-lg-6 col-md-6"><span><img src="{{URL::asset('images/sidebar/patient.png')}}" class="patient"></span>add<span><img src="{{URL::asset('images/sidebar/patient_add.png')}}" class="image"></span></div></a>
+    </div>
+    <div class="row admin">
+        <a class="sidebar-item" href="#">
+            <div class="col-lg-6 col-md-6 practice font_change">Practice</div><div class="col-lg-6 col-md-6"><span><img src="{{URL::asset('images/sidebar/practice.png')}}" class="patient"></span>add<span><img src="{{URL::asset('images/sidebar/practice_add.png')}}" class="image"></span></div></a>
+    </div>
+    <div class="row admin">
+        <a class="sidebar-item" href="#">
+            <div class="col-lg-6 col-md-6 users font_change">Users</div><div class="col-lg-6 col-md-6"><span><img src="{{URL::asset('images/sidebar/users.png')}}" class="patient"></span>add<span><img src="{{URL::asset('images/sidebar/users_add.png')}}" class="image"></span></div></a>
+    </div>
+    <div class="row admin">
+        <a class="sidebar-item" href="#">
+            <div class="col-lg-6 col-md-6 files font_change">Files</div><div class="col-lg-6 col-md-6"><span><img src="{{URL::asset('images/sidebar/files.png')}}" class="patient"></span>add<span><img src="{{URL::asset('images/sidebar/files_add.png')}}" class="image"></span></div></a>
+    </div>
+</div>
+
 
 
 <!--sidebar end-->
