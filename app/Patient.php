7c4ba1aaa16356ee25670ae891a8e1c29a0dea37
pankaja455 <pankaja455@gmail.com>
2016-01-05 13:39:59 +0530

    added search query for providers

--- app/Patient.php
+++ app/Patient.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    //
+
+
+    public static function getFilteredData($filters)
+    {
+       return self::where(function ($query) use ($filters) {
+            foreach($filters as $filter) {
+                $query->where(function ($query) use ($filter) {
+                    switch($filter['type']){
+                        case 'name' :
+                            $query->where('firstname', $filter['value'])
+                                ->orWhere('middlename', $filter['value'])
+                                ->orWhere('lastname', $filter['value']);
+                            break;
+
+                        case 'ssn' :
+                            $query->where('lastfourssn', $filter['value']);
+                            break;
+
+                        case 'all' :
+                            $query->where('firstname', $filter['value'])
+                                ->orWhere('middlename', $filter['value'])
+                                ->orWhere('lastname', $filter['value'])
+                                ->orWhere('lastfourssn', $filter['value']);
+
+                            break;
+                    }
+                            });
+                }
+            })
+            ->get();
+
+    }
+
+
+
+
 }
