9f1c9dddba5558a3d12d2e85b58b712fb26b1b6e
Nishanth <kd@coloredcow.in>
2016-01-06 18:44:43 +0530

    past provider stub

--- public/js/practice.js
+++ public/js/practice.js
@@ -67,8 +67,8 @@ $(document).ready(function () {
 
 //function that displays the providers near patients
 function showPreviousProvider() {
-    var provider_list = new Array("2536", "ColoredCow", "Tushar", "Gurgaon", "Eyes");
-    var content = '<div class="col-xs-12 list_seperator" data-id="' + provider_list[0] + '"><div class="row"><div class="col-xs-12">' + provider_list[1] + '<br> ' + provider_list[2] + ' </div><div class="col-xs-6">' + provider_list[3] + ' </div><div class="col-xs-6">' + provider_list[4] + ' </div></div></div>';
+    var provider_list = new Array("1", "John Doe", "Becker Eye", "Gurgaon", "Eyes");
+    var content = '<div class="col-xs-12 list_seperator" data-id="' + provider_list[0] + '"><div class="row"><div class="col-xs-12">' + provider_list[1] + '<br> ' + provider_list[2] + ' </div><div class="col-xs-6"> </div><div class="col-xs-6"> </div></div></div>';
     $('.provider_near_patient_list').html(content);
     $('.provider_near').removeClass('glyphicon-chevron-right');
     $('.provider_near').addClass('glyphicon-chevron-down');
@@ -76,8 +76,8 @@ function showPreviousProvider() {
 
 //function that displays the previous providers of the patients
 function showProviderNear() {
-    var provider_list = new Array("2536", "ColoredCow", "Tushar", "Gurgaon", "Eyes");
-    var content = '<div class="col-xs-12 list_seperator" data-id="' + provider_list[0] + '"><div class="row"><div class="col-xs-12">' + provider_list[1] + '<br> ' + provider_list[2] + ' </div><div class="col-xs-6">' + provider_list[3] + ' </div><div class="col-xs-6">' + provider_list[4] + ' </div></div></div>';
+    var provider_list = new Array("1", "John Doe", "Becker Eye", "Gurgaon", "Eyes");
+    var content = '<div class="col-xs-12 list_seperator" data-id="' + provider_list[0] + '"><div class="row"><div class="col-xs-12">' + provider_list[1] + '<br> ' + provider_list[2] + ' </div><div class="col-xs-6"></div><div class="col-xs-6"> </div></div></div>';
     $('.previous_provider_patient_list').html(content);
     $('.provider_previous').removeClass('glyphicon-chevron-right');
     $('.provider_previous').addClass('glyphicon-chevron-down');
