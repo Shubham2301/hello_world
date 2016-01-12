daf953490044d8d592dd740efbc4d10508c28fbd
bohraji <tusharbohra01@gmail.com>
2016-01-12 16:45:11 +0530

    Change Location of "Select Provider" button to top right corner #20

--- resources/assets/js/patient.js
+++ resources/assets/js/patient.js
@@ -16,10 +16,10 @@ $(document).ready(function () {
     });
 
     $('#change_patient_button').on('click', function () {
-
         $('.patient_list').addClass('active');
         $('.patient_info').removeClass('active');
-
+        $('#select_provider_button').removeClass('active');
+        $('#select_provider_button').attr('data-id', 0);
     });
 
     $('#select_provider_button').on('click', function () {
@@ -38,76 +38,44 @@ $(document).ready(function () {
     });
     $('.search_filter').on('click', '.remove_option', function () {
         $(this).parent().remove();
-
     });
 
 
-    $('.lastseenby_show').on('click',function(){
-
-       $('.lastseen_content').toggleClass('active');
-
-        if($('.lastseen_content').hasClass('active'))
-            {
-                $('.lastseenby_icon').removeClass('glyphicon-chevron-right');
-                $('.lastseenby_icon').addClass('glyphicon-chevron-down');
-            }
-        else
-            {
-                $('.lastseenby_icon').removeClass('glyphicon-chevron-down');
-                $('.lastseenby_icon').addClass('glyphicon-chevron-right');
-
-            }
-
-
+    $('.lastseenby_show').on('click', function () {
+        $('.lastseen_content').toggleClass('active');
+        if ($('.lastseen_content').hasClass('active')) {
+            $('.lastseenby_icon').removeClass('glyphicon-chevron-right');
+            $('.lastseenby_icon').addClass('glyphicon-chevron-down');
+        } else {
+            $('.lastseenby_icon').removeClass('glyphicon-chevron-down');
+            $('.lastseenby_icon').addClass('glyphicon-chevron-right');
+        }
     });
 
-
-    $('.referredby_show').on('click',function(){
-
-       $('.referredby_content').toggleClass('active');
-
-        if($('.referredby_content').hasClass('active'))
-            {
-                $('.referredby_icon').removeClass('glyphicon-chevron-right');
-                $('.referredby_icon').addClass('glyphicon-chevron-down');
-            }
-        else
-            {
-                $('.referredby_icon').removeClass('glyphicon-chevron-down');
-                $('.referredby_icon').addClass('glyphicon-chevron-right');
-
-            }
-
-
+    $('.referredby_show').on('click', function () {
+        $('.referredby_content').toggleClass('active');
+        if ($('.referredby_content').hasClass('active')) {
+            $('.referredby_icon').removeClass('glyphicon-chevron-right');
+            $('.referredby_icon').addClass('glyphicon-chevron-down');
+        } else {
+            $('.referredby_icon').removeClass('glyphicon-chevron-down');
+            $('.referredby_icon').addClass('glyphicon-chevron-right');
+        }
     });
 
-    $('.insurance_provider_show').on('click',function(){
-
-       $('.insurance_provider_content').toggleClass('active');
-
-        if($('.insurance_provider_content').hasClass('active'))
-            {
-                $('.insurance_provider_icon').removeClass('glyphicon-chevron-right');
-                $('.insurance_provider_icon').addClass('glyphicon-chevron-down');
-            }
-        else
-            {
-                $('.insurance_provider_icon').removeClass('glyphicon-chevron-down');
-                $('.insurance_provider_icon').addClass('glyphicon-chevron-right');
-
-            }
-
-
+    $('.insurance_provider_show').on('click', function () {
+        $('.insurance_provider_content').toggleClass('active');
+        if ($('.insurance_provider_content').hasClass('active')) {
+            $('.insurance_provider_icon').removeClass('glyphicon-chevron-right');
+            $('.insurance_provider_icon').addClass('glyphicon-chevron-down');
+        } else {
+            $('.insurance_provider_icon').removeClass('glyphicon-chevron-down');
+            $('.insurance_provider_icon').addClass('glyphicon-chevron-right');
+        }
     });
-
-
-
 });
 
-
-
 function showPatientInfo(data) {
-
     $('.patient_list').removeClass('active');
     $('.patient_info').addClass('active');
     $('#patient_name').text(data.firstname);
@@ -148,7 +116,7 @@ function getPatients(formData) {
     $('.patient_list').addClass('active');
     $('.patient_info').removeClass('active');
     $('#select_provider_button').removeClass('active');
-     $('#select_provider_button').attr('data-id', 0);
+    $('#select_provider_button').attr('data-id', 0);
 
     var tojson = JSON.stringify(formData);
     $.ajax({
@@ -188,22 +156,17 @@ function getsearchtype() {
         searchdata.push({
             "type": stype,
             "value": name,
-
         });
-
     });
     return searchdata;
 }
 
 function getOptionContent(type, value) {
     var content = '<div class="search_filter_item"><span class="item_type">' + type + '</span>:<span class="item_value">' + value + '</span><span class="remove_option">x</span></div>';
-
     return content;
-
 }
 
 function selectProvider(id) {
-
     $('#form_patient_id').val(id);
     $('#form_select_provider').submit();
 }
