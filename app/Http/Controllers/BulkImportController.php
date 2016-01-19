2c0af656b6a20bd108b36ba8ea956f012ecd9982
pankaja455 <pankaja455@gmail.com>
2016-01-19 23:58:57 +0530

    get uploaded csv file data

--- app/Http/Controllers/BulkImportController.php
+++ app/Http/Controllers/BulkImportController.php
@@ -5,27 +5,25 @@ namespace myocuhub\Http\Controllers;
 use Illuminate\Http\Request;
 use myocuhub\Models\Practice;
 use myocuhub\Models\PracticeLocation;
-
-
+use Maatwebsite\Excel\Facades\Excel;
 
 class BulkImportController extends Controller
 {
 
-    public function index(){
-
-       // $Practicedata =  \::where('collegeid','=',Session::get('user')->collegeid)->lists('classname', 'id');
-        $practicedata = Practice::all()->lists('name','id');
-        return view('patient.import')->with('data',$practicedata);
+    public function index()
+    {
+        $practicedata = Practice::all()->lists('name', 'id');
+        return view('patient.import')->with('data', $practicedata);
     }
 
-    public function getLocations(Request $request){
+    public function getLocations(Request $request)
+    {
 
         $practice_id = $request->input('practice_id');
         $locations = PracticeLocation::where('practice_id', $practice_id)->get();
         $data = [];
         $i=0;
-        foreach ($locations as $location)
-        {
+        foreach ($locations as $location) {
             $data[$i]['id']= $location->id;
             $data[$i]['name']= $location->locationname;
             $i++;
@@ -37,15 +35,42 @@ class BulkImportController extends Controller
 
     }
 
-    public function importPatientsCsv(Request $request){
-        if ($request->hasFile('photo')) {
+    public function importPatientsCsv(Request $request)
+    {
+        if ($request->hasFile('patient_csv')) {
+            $excels = Excel::load($request->file('patient_csv'))->get();
+            $patientdata = [];
+            foreach ($excels as $sheet) {
+                $title = $sheet->getTitle();
+                switch($title)
+                {
+                    case 'Users':
+                        foreach ($sheet as $data) {
+                            echo $data->type.'</br>';
+                        }
+                        echo '</br>';
+                        break;
+                    case 'Accounts':
+                        foreach ($sheet as $data) {
+                            if (array_filter($data->toArray())) {
+                                echo $data->practice_name.'</br>';
+                            }
 
+                        }
+                        echo '</br>';
+                        break;
+                    case 'Patients':
+                        foreach ($sheet as $data) {
+                            if (array_filter($data->toArray())) {
+                                echo $data->patient_name.'</br>';
+                            }
+
+                        }
+                        echo '</br>';
+                        break;
+                }
+
+            }
         }
-        dd($request->input());
-
-
     }
-
-
-
 }
