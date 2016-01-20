720ff30422a95a06eb1dbf87c4a54a1e682c901e
bohraji <tusharbohra01@gmail.com>
2016-01-20 17:13:16 +0530

    Search functionality design added in c3 console and sidebar updated.

--- resources/views/careconsole/index.blade.php
+++ resources/views/careconsole/index.blade.php
@@ -15,8 +15,36 @@
         <div class="section-header">
             Overview
             <div class="search" id="care_console_search">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="glyphicon glyphicon-search" aria-hidden="true"></span></div>
+                <input type="text" id="search_data">
+                <span class="glyphicon glyphicon-chevron-left active" aria-hidden="true" id="search_bar_open"></span>
+                <span class="glyphicon glyphicon-search" aria-hidden="true" id="search_do"></span>
+                <div class="search_result">
+                    <div class="search_result_row row">
+                        <div class="col-xs-2 circle red"></div>
+                        <div class="col-xs-10 search_result_row_text">
+                            <p class="result_title">Past Appointments</p>
+                        </div>
+                    </div>
+                    <div class="search_result_row row">
+                        <div class="col-xs-10 col-xs-offset-2 search_result_row_text">
+                            <p class="result_title">Scheduled To</p>
+                            <p class="result_text">Daniel Garibaldi</p>
+                        </div>
+                    </div>
+                    <div class="search_result_row row">
+                        <div class="col-xs-10 col-xs-offset-2 search_result_row_text">
+                            <p class="result_title">Appointment Date</p>
+                            <p class="result_text">January 8, 2016</p>
+                        </div>
+                    </div>
+                    <div class="search_result_row row">
+                        <div class="col-xs-10 col-xs-offset-2 search_result_row_text">
+                            <p class="result_title">Take action
+                            <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
