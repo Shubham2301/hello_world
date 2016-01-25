2751fe59d9cb0314f2a7729db6fe63574ff60762
bohraji <tusharbohra01@gmail.com>
2016-01-25 13:15:31 +0530

    Add Title in middle box for the consistency on schedule appointment page #73

--- resources/views/appointment/index.blade.php
+++ resources/views/appointment/index.blade.php
@@ -38,20 +38,21 @@
             <div class="row appointment_info center-align">
                 <div class="col-xs-4">
                     <div class="appointment_info_box patient">
-                        <h4>Patient</h4>
+                        <h4><strong>Patient</strong></h4>
                         <img src="{{URL::asset('images/provider.png')}}"><br><br><br>
-                        <h4>{{ $data['patient_name'] }}</h4>
+                        <h4><strong>{{ $data['patient_name'] }}</strong></h4>
                     </div>
                 </div>
                 <div class="col-xs-4">
                     <div class="appointment_info_box schedule">
-                        <h4>5PM,<br>Tuesday,<br>5-Jan-2015</h4><br><br>
-                        <h4 class="schedule_place" style="padding-top:10px;">Rockville Center</h4>
+                        <h4><strong>Details</strong></h4>
+                        <h4>5PM,<br>Tuesday,<br>5-Jan-2015</h4><br>
+                        <h4 class="schedule_place" style="padding-top:6px;"><strong>Rockville Center</strong></h4>
                     </div>
                 </div>
                 <div class="col-xs-4">
                     <div class="appointment_info_box provider">
-                        <h4>Provider</h4>
+                        <h4><strong>Provider</strong></h4>
                         <img src="{{URL::asset('images/provider.png')}}"><br><br><br>
                         <h4><strong>{{ $data['provider_name'] }}</strong><br>{{ $data['practice_name'] }}</h4>
                     </div>
