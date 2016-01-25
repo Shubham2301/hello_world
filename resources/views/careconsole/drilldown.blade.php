9fbe9758bbd52ea18fe74775a6341daa93d0b698
Nishanth <kd@coloredcow.in>
2016-01-25 19:42:47 +0530

    Custom Dropdown

--- resources/views/careconsole/drilldown.blade.php
+++ resources/views/careconsole/drilldown.blade.php
@@ -1,7 +1,10 @@
 <div class="drilldown">
     <div class="section-header">
     </div>
-    <div class="subsection-header"><p></p><div class="circle red"></div></div>
+    <div class="subsection-header">
+        <p></p>
+        <div class="circle red"></div>
+    </div>
     <div class="row search_header">
         <div class="col-xs-2 drilldown_header_item">
             Patient Name
