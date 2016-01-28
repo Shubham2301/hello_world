f0ff4da5d9f6e9b757c875a57032d671508673d1
Nishanth <kd@coloredcow.in>
2016-01-28 16:15:47 +0530

    Migrations for Audit Logs

---
+++ database/migrations/2016_01_28_103104_audit_logs.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AuditLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('audit_trail');
+
+        Schema::create('audit_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('action', 200);
+            $table->string('file', 200)->nullable();
+            $table->text('description', 200)->nullable();
+            $table->string('ip', 200)->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('audit_logs', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('audit_logs');
+    }
+}
