3b1e25337deba517df9cea61f5c842d3290099ea
Nishanth <kd@coloredcow.in>
2016-02-10 14:35:55 +0530

    Add Clinical Protocol screen when pressing Disease tile #47

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.master') @section('title', 'My Ocuhub') @section('imports')
 <link rel="stylesheet" type="text/css" href="{{elixir('css/referral.css')}}">
 <script type="text/javascript" src="{{elixir('js/referraltype.js')}}"></script>
-@endsection @section('sidebar') @include('layouts.sidebar') @endsection @section('content')
-
-
+@endsection
+@section('sidebar')
+@include('layouts.sidebar')
+@endsection
+@section('content')
 <div class="content-section active" id="referral_types">
-   <p class="message">Select type of patient you are referring</p>
-    <div class="row">
-        {!! Form::open(array('url' => '/patients', 'method' => 'GET', 'id' => 'form_select_patient')) !!} {!! Form::hidden('referraltype_id', '0', array('id' => 'form_referraltype_id')) !!} {!! Form::hidden('action', 'schedule_appointment', array('id' => 'form_referraltype_id')) !!} {!! Form::close() !!}
-        <div class="referral_tiles" id="referraltypes_list">
-        </div>
-    </div>
+	<p class="message">Select type of patient you are referring</p>
+	<div class="row">
+		{!! Form::open(array('url' => '/patients', 'method' => 'GET', 'id' => 'form_select_patient')) !!}
+		{!! Form::hidden('referraltype_id', '0', array('id' => 'form_referraltype_id')) !!}
+		{!! Form::hidden('action', 'schedule_appointment', array('id' => 'form_referraltype_id')) !!}
+		{!! Form::close() !!}
+		<div class="referral_tiles" id="referraltypes_list"></div>
+	</div>
 </div>
-
 @endsection
