9b5642435c3196dbf7cb760d0da3d1747886682a
Nishanth <kd@coloredcow.in>
2016-02-19 15:49:23 +0530

    Separate patient listing from drilldown view

---
+++ resources/views/careconsole/listing.blade.php
@@ -0,0 +1,23 @@
+<div class="row search_header">
+        <div class="col-xs-2 drilldown_header_item">
+            Patient Name
+        </div>
+        <div class="col-xs-2 drilldown_header_item">
+            Patient Phone
+        </div>
+        <div class="col-xs-2 drilldown_header_item">
+            Request Received
+        </div>
+        <div class="col-xs-2 drilldown_header_item">
+            Appointment Date
+        </div>
+        <div class="col-xs-2 drilldown_header_item">
+            Scheduled To
+        </div>
+        <div class="col-xs-2 drilldown_header_item">
+            Actions
+        </div>
+</div>
+<div class="drilldown_content">
+
+</div>
