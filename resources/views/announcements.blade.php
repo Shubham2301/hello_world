b56b37a2329610afe7feca35565f599ae9d0e86e
bohraji <tusharbohra01@gmail.com>
2016-02-20 17:55:24 +0530

    show announcement final view.

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -9,102 +9,14 @@
             <span class="announcement_tabs" id="make_announcements">Make</span>
         </span>
         <span class="announcement_navbar_right">
+            <button class="back-button" >Back</button>
             <button class="mark_as_read" >Mark as read</button>
-            <span class=""><img class="cancel_image" src="{{URL::asset('images/delete.png')}}"></span>
+            <span class="delete" data-toggle="tooltip" title="Delete" data-placement="bottom">
+                <img class="cancel_image" src="{{URL::asset('images/delete-natural.png')}}">
+                <img class="cancel_image-hover" src="{{URL::asset('images/delete-natural-hover.png')}}">
+            </span>
         </span>
     </span>
     <span class="announcement_content">
-<!--
-        <span class="announcement_list_item arial">
-            <span class="item_header list_item_section">
-               <span class="item_left">
-                    <input type="checkbox" class="select_item" name="checkbox" data-id="0">
-                </span>
-                <span class="item_right list_item_section">
-                    <span class="title">From: Office of Care Coordination</span>
-                    <span class="date">10-Feb-2016</span>
-                </span>
-            </span>
-            <span class="item_subject list_item_section">
-                <span class="glyphicon glyphicon-exclamation-sign item_left"></span>
-                <span class="title arial_bold item_right">
-                    <span class="item_link" data-id="0">Protocol Change - Referral Protocol</span>
-                </span>
-            </span>
-            <span class="item_text list_item_section">
-                <span class="item_left"></span>
-                <span class="item_right">
-                Effective January 4, all office will begin using.
-                </span>
-            </span>
-            <span class="item_text list_item_section">
-                <span class="item_left"></span>
-                <span class="item_right">
-                    <span class="section_separator">
-                    </span>
-                </span>
-            </span>
-        </span>
-        <span class="announcement_list_item arial">
-            <span class="item_header list_item_section">
-               <span class="item_left">
-                    <input type="checkbox" name="checkbox" class="select_item" data-id="1">
-                </span>
-                <span class="item_right list_item_section">
-                    <span class="title">From: Office of Care Coordination</span>
-                    <span class="date">10-Feb-2016</span>
-                </span>
-            </span>
-            <span class="item_subject list_item_section">
-                <span class="glyphicon glyphicon-exclamation-sign item_left"></span>
-                <span class="arial_bold item_right title">
-                    <span class="item_link" data-id="1">Protocol Change - Referral Protocol</span>
-                </span>
-            </span>
-            <span class="item_text list_item_section">
-                <span class="item_left"></span>
-                <span class="item_right text">
-                Effective January 4, all office will begin using.
-                </span>
-            </span>
-            <span class="item_text list_item_section">
-                <span class="item_left"></span>
-                <span class="item_right">
-                    <span class="section_separator">
-                    </span>
-                </span>
-            </span>
-        </span>
-        <span class="announcement_list_item arial">
-            <span class="item_header list_item_section">
-               <span class="item_left">
-                    <input type="checkbox" name="checkbox" class="select_item" data-id="2">
-                </span>
-                <span class="item_right list_item_section">
-                    <span class="title">From: Office of Care Coordination</span>
-                    <span class="date">10-Feb-2016</span>
-                </span>
-            </span>
-            <span class="item_subject list_item_section">
-                <span class="item_left"></span>
-                <span class="arial_bold item_right title" >
-                    <span class="item_link" data-id="2">Protocol Change - Referral Protocol</span>
-                </span>
-            </span>
-            <span class="item_text list_item_section">
-                <span class="item_left"></span>
-                <span class="item_right text">
-                Effective January 4, all office will begin using.
-                </span>
-            </span>
-            <span class="item_text list_item_section">
-                <span class="item_left"></span>
-                <span class="item_right">
-                    <span class="section_separator">
-                    </span>
-                </span>
-            </span>
-        </span>
--->
     </span>
 </div>
