b1a33ae9b24e6a4cab06026ed2111e42361312dc
pankaja455 <pankaja455@gmail.com>
2016-02-24 00:19:28 +0530

    fixed issue #214

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,19 +1,19 @@
 @if(Auth::check())
 <div class="sidebar_user_info center arial_bold">
-	<h4>{{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</h4>
+    <h4>{{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</h4>
 </div>
 <div class="sidebar_menu center">
-	<img src="{{URL::asset('images/users/user_'. Auth::user()->id .'.jpg')}}" class="profile_img_sidebar" onerror="this.src = '{{URL::asset('images/sidebar/care_coordinator.png')}}'">
+    <img src="{{URL::asset('images/users/user_'. Auth::user()->id .'.jpg')}}" class="profile_img_sidebar" onerror="this.src = '{{URL::asset('images/sidebar/care_coordinator.png')}}'">
 </div>
 <ul class="sidebar_item_list arial">
-	<?php $menus = \myocuhub\Models\Menu::renderForUser(Auth::user())?>
-	@foreach($menus as $menu)
-	<li id="menu-{{ $menu->name }}" class="sidebar_menu_item {{ array_key_exists('directmail_active', $data) ? 'active' : '' }}">
-		<a class="sidebar_item" href="{{ $menu->url }}">
-			<span class="menu_item_icon"><img src="{{URL::asset($menu->icon_path.'.png')}}" class="image"></span>
-			<span class="menu_item_label">{{ $menu->display_name }}</span>
-		</a>
-	</li>
-	@endforeach
+    <?php $menus=\ myocuhub\Models\Menu::renderForUser(Auth::user())?> @foreach($menus as $menu)
+    <li id="menu-{{ $menu->name }}" class="sidebar_menu_item {{ array_key_exists('directmail_active', $data) ? 'active' : ''
+										   }}" onclick="location.href = '{{$menu->url}}'">
+        <a class="sidebar_item" href="{{ $menu->url }}">
+            <span class="menu_item_icon"><img src="{{URL::asset($menu->icon_path.'.png')}}" class="image"></span>
+            <span class="menu_item_label">{{ $menu->display_name }}</span>
+        </a>
+    </li>
+    @endforeach
 </ul>
 @endif
