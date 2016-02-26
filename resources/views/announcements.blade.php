cdbf936750908436bad8020d8134225d7814c056
bohraji <tusharbohra01@gmail.com>
2016-02-26 12:31:18 +0530

    announcement view updates

--- resources/views/announcements.blade.php
+++ resources/views/announcements.blade.php
@@ -1,19 +1,25 @@
 <div class="announcement_box">
-    <span class="announcement_row arial_bold title">
-        <span>Announcements</span>
-        <span class="glyphicon glyphicon-remove" id="close_announcement"></span>
-    </span>
-    <span class="announcement_row announcement_navbar arial">
-        <span class="announcement_navbar_left">
-            <span class="announcement_tabs" id="show_announcements">View</span>
-            <span class="announcement_tabs" id="make_announcements">Make</span>
+    <span class="announcement_row arial_bold">
+        <span class="left"></span>
+        <span class="right title">
+            <span>Announcements</span>
+            <span class="glyphicon glyphicon-remove" id="close_announcement"></span>
         </span>
-        <span class="announcement_navbar_right">
-            <button class="back-button" >Back</button>
-            <button class="mark_as_read" >Mark as read</button>
-            <span class="delete" data-toggle="tooltip" title="Delete" data-placement="bottom">
-                <img class="cancel_image" src="{{URL::asset('images/delete-natural.png')}}">
-                <img class="cancel_image-hover" src="{{URL::asset('images/delete-natural-hover.png')}}">
+    </span>
+    <span class="announcement_row">
+        <span class="left"></span>
+        <span class="right announcement_navbar arial">
+            <span class="announcement_navbar_left">
+                <span class="announcement_tabs" id="show_announcements">View</span>
+                <span class="announcement_tabs" id="make_announcements">Make</span>
+            </span>
+            <span class="announcement_navbar_right">
+                <button class="back-button" >Back</button>
+                <button class="mark_as_read" >Mark as read</button>
+                <span class="delete" data-toggle="tooltip" title="Delete" data-placement="bottom">
+                    <img class="cancel_image" src="{{URL::asset('images/delete-natural.png')}}">
+                    <img class="cancel_image-hover" src="{{URL::asset('images/delete-natural-hover.png')}}">
+                </span>
             </span>
         </span>
     </span>
