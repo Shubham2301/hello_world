72f6a0e2b821281ea1d1b56643b3f12eec5dca13
Nishanth <kd@coloredcow.in>
2016-02-27 11:58:30 +0530

    Critical - Sign out of SES #227

--- resources/views/directmail/index.blade.php
+++ resources/views/directmail/index.blade.php
@@ -42,6 +42,8 @@
         <form id="ocuhubSESFm" action="{{ $ses['sso_logon_url'] }}" method="post" target="_blank">
             <input id="id_token" type='hidden' name='token' value="" />
         </form>
+        <!-- <form id="ses_logoout_form" action="{{ $ses['sso_logoff_url'] }}" method="get" target="_blank">
+        </form> -->
 
         <iframe id="ocuhubSESiframeId" name="ocuhubSESiframe" src="" frameborder="0" style="display:none;position:relative;width: 100%;flex: 1 1 auto;"></iframe>
 
@@ -174,13 +176,6 @@
         if (!window.location.search) {
             document.getElementById("getCodeBtn").click();
         }
-        /*if (window.location.search){
-            $(window).on("beforeunload", function() {
-                document.getElementById('ocuhubSESiframeId').src = '{{ $ses['sso_logoff_url'] }}';
-                document.getElementById('ocuhubSESiframeId').src = 'http://local.myocuhub.com/file_exchange';
-                return "Signing Off From Direct Mail";
-            });
-        }*/
     </script>
 
     @endif
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,47 +8,53 @@
         <span class="arial_bold dark_header_text">&nbsp;support@ocuhub.com&nbsp;</span>
         <span class="arial_bold light_header_text">&nbsp;Call:</span>
         <span class="arial_bold dark_header_text">&nbsp;(888)277-3365</span>
-        <div class="header-user-item"><a href="/auth/logout">LOGOUT</a></div>
+        <div class="header-user-item ocuhub_sso_logoff">
+            <span onclick="singleSignOff()">LOGOUT</span>
+        </div>
+        <div class="ocuhub_sso_logoff">
+            <form id="ses_logout_form" target="ses_logout_iframe" action="https://direct.ocuhub.com/sesidpserver/connect/endsession" method="GET"></form>
+            <iframe id="ses_logout_iframe" name="ses_logout_iframe" src="" frameborder="0" style="display:none;"></iframe>
+        </div>
     </div>
 </div>
 @endif
 <div class="row height header">
-    <div class="col-xs-3 hidden-xs header-left">
-		<div class="col-xs-5 network_logo" >
+<div class="col-xs-3 hidden-xs header-left">
+    <div class="col-xs-5 network_logo" >
         @if(Auth::check())
-		<img src="{{URL::asset('images/networks/default_network_logo.jpg')}}" class="img-responsive">
+        <img src="{{URL::asset('images/networks/default_network_logo.jpg')}}" class="img-responsive">
         @endif
-		</div>
     </div>
-    <div class="col-xs-12 col-sm-9 header-right">
-        <div class="col-xs-3 header-logo">
-            <img src="{{URL::asset('images/ocuhub-logo.png')}}" class="img-responsive">
-            <div id="loader-container">
-                {{--
-                <p id="loadingText">Loading</p> --}}
+</div>
+<div class="col-xs-12 col-sm-9 header-right">
+    <div class="col-xs-3 header-logo">
+        <img src="{{URL::asset('images/ocuhub-logo.png')}}" class="img-responsive">
+        <div id="loader-container">
+            {{--
+            <p id="loadingText">Loading</p> --}}
+        </div>
+    </div>
+    <div class="col-xs-9 header-menu">
+        @if(Auth::check())
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ocuhub-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
             </div>
-        </div>
-        <div class="col-xs-9 header-menu">
-            @if(Auth::check())
-            <nav class="navbar navbar-default" role="navigation">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ocuhub-navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="ocuhub-navbar-collapse">
-                    <ul class="nav navbar-nav arial_bold header_navbar_items">
-                        <li class="menu-item"><a href="/home">HOME</a></li>
-                        <li class="menu-item"><a href="/techsupport">TECH SUPPORT</a></li>
-                    </ul>
-                </div>
-            </nav>
-            @endif
-        </div>
+            <div class="collapse navbar-collapse" id="ocuhub-navbar-collapse">
+                <ul class="nav navbar-nav arial_bold header_navbar_items">
+                    <li class="menu-item"><a href="/home">HOME</a></li>
+                    <li class="menu-item"><a href="/techsupport">TECH SUPPORT</a></li>
+                </ul>
+            </div>
+        </nav>
+        @endif
     </div>
-    <!--
-    -->
+</div>
+<!--
+-->
 </div>
