814d64e9011e303ad4abb2cf0d1d4ba4b8c89def
Nishanth <kd@coloredcow.in>
2016-02-29 12:01:33 +0530

    Need to Flag Patients as High Priority #193

--- app/Http/Controllers/BulkImportController.php
+++ app/Http/Controllers/BulkImportController.php
@@ -120,6 +120,7 @@ class BulkImportController extends Controller {
 								$careconsole->import_id = $importHistory->id;
 								$careconsole->patient_id = $patient->id;
 								$careconsole->stage_id = 1;
+								$careconsole->stage_id = $data['priority'];
 								$date = new DateTime();
 								$console->stage_updated_at = $date->format('Y-m-d H:m:s');
 								$careconsole->save();
