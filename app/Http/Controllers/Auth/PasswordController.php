cd52e8c6bfb02d5db486fda52cd15c70bb727687
Nishanth <kd@coloredcow.in>
2016-03-03 22:00:07 +0530

    Password Reset

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -2,31 +2,30 @@
 
 namespace myocuhub\Http\Controllers\Auth;
 
-use myocuhub\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use myocuhub\Http\Controllers\Controller;
 
-class PasswordController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+class PasswordController extends Controller {
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reset Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller is responsible for handling password reset requests
+	| and uses a simple trait to include this behavior. You're free to
+	| explore this trait and override any methods you wish to tweak.
+	|
+	 */
 
-    use ResetsPasswords;
+	use ResetsPasswords;
 
-    /**
-     * Create a new password controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+	/**
+	 * Create a new password controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware('guest');
+	}
+
 }
