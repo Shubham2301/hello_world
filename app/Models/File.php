3d938c3992de42c9414be26ea359f50341e2bba9
Nishanth <kd@coloredcow.in>
2016-03-07 18:06:51 +0530

    Use statement revision

--- app/Models/File.php
+++ app/Models/File.php
@@ -3,36 +3,31 @@
 namespace myocuhub\Models;
 
 use Auth;
-
 use Illuminate\Database\Eloquent\Model;
-
 use myocuhub\Models\FileHistory;
-use myocuhub\Model\FileShare;
+use myocuhub\Models\FileShare;
 
-class File extends Model
-{
-    public function history()
-    {
-    	return $this->hasMany(FileHistory::class);
-    }
+class File extends Model {
+	public function history() {
+		return $this->hasMany(FileHistory::class);
+	}
 
-    public function sharedwith()
-    {
-    	return $this->hasMany(FileShare::class);
-    }
+	public function sharedwith() {
+		return $this->hasMany(FileShare::class);
+	}
 
-    public static function getFiles($folder_id=0, $active=1) {
+	public static function getFiles($folder_id = 0, $active = 1) {
 
-    	if($folder_id == null){
-    		return File::where('status', '=', $active)
-    			->where('creator_id', '=', Auth::user()->id)
-                ->whereNull('folder_id')
-    			->orderBy('title', 'asc')->get();
-    	}
+		if ($folder_id == null) {
+			return File::where('status', '=', $active)
+				->where('creator_id', '=', Auth::user()->id)
+				->whereNull('folder_id')
+				->orderBy('title', 'asc')->get();
+		}
 
-    	return File::where('status', '=', $active)
-    		->where('creator_id', '=', Auth::user()->id)
-    		->where('folder_id', '=', $folder_id)
-    		->orderBy('title', 'asc')->get();
-    }
+		return File::where('status', '=', $active)
+			->where('creator_id', '=', Auth::user()->id)
+			->where('folder_id', '=', $folder_id)
+			->orderBy('title', 'asc')->get();
+	}
 }
