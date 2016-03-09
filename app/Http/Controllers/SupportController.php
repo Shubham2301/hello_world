fb9cf7f651fbff104e27a8d0440195745d5efdea
bohraji <tusharbohra01@gmail.com>
2016-03-09 08:53:24 +0530

    Tech support link broken #317

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -9,34 +9,39 @@ use myocuhub\Http\Controllers\Controller;
 
 class SupportController extends Controller
 {
-    $data = array();
     public function techSupportIndex()
     {
+        $data = array();
         return view('support.support')->with('data', $data);
     }
 
     public function contactusIndex()
     {
+        $data = array();
         return view('support.contactus')->with('data', $data);
     }
 
     public function investorsIndex()
     {
+        $data = array();
         return view('support.investors')->with('data', $data);
     }
 
     public function privacyIndex()
     {
+        $data = array();
         return view('support.privacy')->with('data', $data);
     }
 
     public function sitemapIndex()
     {
+        $data = array();
         return view('support.sitemap')->with('data', $data);
     }
 
     public function termsIndex()
     {
+        $data = array();
         return view('support.terms')->with('data', $data);
     }
 
