256e260656016d5586b226fa95b0ebe712c36443
Nishanth <kd@coloredcow.in>
2016-03-09 11:34:06 +0530

    network id and user level in session on Auth.Login event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,27 +5,25 @@ namespace myocuhub\Providers;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider
-{
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        'myocuhub\Events\MakeAuditEntry' => [],
-    ];
+class EventServiceProvider extends ServiceProvider {
+	/**
+	 * The event listener mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $listen = [
+		'myocuhub\Events\MakeAuditEntry' => [],
+	];
 
-    /**
-     * Register any other events for your application.
-     *
-     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
-     * @return void
-     */
-    public function boot(DispatcherContract $events)
-    {
-        parent::boot($events);
+	/**
+	 * Register any other events for your application.
+	 *
+	 * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+	 * @return void
+	 */
+	public function boot(DispatcherContract $events) {
+		parent::boot($events);
 
-        //
-    }
+		//
+	}
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,9 @@
 <div class="row height header">
     <div class="col-xs-3 hidden-xs header-left">
         <div class="col-xs-8 col-xs-offset-2 network_logo">
-            @if(Auth::check())
-            <img src="{{URL::asset('images/networks/network_'. Auth::user()->getNetwork(Auth::user()->id)->id .'.png')}}" onerror="this.src = '{{URL::asset('images/networks/default_network_logo.png')}}'"> @endif
+            @if( Auth::check() && session('user-level') > 1) )
+            <img src="{{URL::asset('images/networks/network_'. Auth::user()->getNetwork(Auth::user()->id)->id .'.png')}}" onerror="this.src = '{{URL::asset('images/networks/default_network_logo.png')}}'">
+            @endif
         </div>
     </div>
     <div class="col-xs-12 col-sm-9 header-right">
@@ -18,10 +19,10 @@
             <nav class="navbar navbar-default" role="navigation">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#ocuhub-navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
                     </button>
                 </div>
                 <div class="collapse navbar-collapse" id="ocuhub-navbar-collapse">
@@ -29,17 +30,17 @@
                         <li class="menu-item"><a href="/home">HOME</a></li>
                         <li class="menu-item"><a href="/techsupport">TECH SUPPORT</a></li>
                         <li class="menu-item"><span class="ocuhub_sso_logoff logout_btn"><span onclick="singleSignOff()">LOGOUT</span></span>
-                        </li>
-                    </ul>
-                    <div class="ocuhub_sso_logoff">
-                        <form id="ses_logout_form" target="ses_logout_iframe" action="https://direct.ocuhub.com/sesidpserver/connect/endsession" method="GET"></form>
-                        <iframe id="ses_logout_iframe" name="ses_logout_iframe" src="" frameborder="0" style="display:none;"></iframe>
-                    </div>
-                </div>
-            </nav>
-            @endif
+                    </li>
+                </ul>
+                <div class="ocuhub_sso_logoff">
+                <form id="ses_logout_form" target="ses_logout_iframe" action="https://direct.ocuhub.com/sesidpserver/connect/endsession" method="GET"></form>
+                <iframe id="ses_logout_iframe" name="ses_logout_iframe" src="" frameborder="0" style="display:none;"></iframe>
+            </div>
         </div>
-    </div>
-    <!--
+    </nav>
+    @endif
+</div>
+</div>
+<!--
 -->
 </div>
