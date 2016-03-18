6fc6761b6bc52da1ce1fba7a11295f996e3154a5
Nishanth <kd@coloredcow.in>
2016-03-18 17:18:07 +0530

    email-patient-icon

--- app/Http/Controllers/TestMailController.php
+++ app/Http/Controllers/TestMailController.php
@@ -7,11 +7,13 @@ use Illuminate\Support\Facades\Mail;
 
 class TestMailController extends Controller {
 	public function testMail() {
+		$data = [];
+		return view('emails.appt-confirmation-patient')->with('data', $data);
 		$user = Auth::user();
 
-		$result = Mail::send('emails.test', ['user' => $user], function ($m) use ($user) {
+		$result = Mail::send('emails.appt-confirmation-patient', ['user' => $user], function ($m) use ($user) {
 			$m->from('support@ocuhub.com', 'Ocuhub');
-			$m->to('kd@coloredcow.in', 'KD')->subject('Test Application');
+			$m->to('kd@coloredcow.in', 'KD')->subject('Your Appointment has been scheduled');
 		});
 		dd($result);
 	}
--- resources/views/emails/appointment-confirmation.blade.php
+++
@@ -1,6 +0,0 @@
-
-<img src="{{asset('images/ocuhub-logo.png')}}" alt="">
-<p>Your appointment has been scheduled.</p>
-
-<img src="http://access.myocuhub.com/images/ocuhub-logo.png" style="width:10em" alt="">
-<p>Your appointment has been scheduled.</p>
