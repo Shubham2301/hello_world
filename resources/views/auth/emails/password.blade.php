a353e39b9a182fd963a96b8fc9cf79e2da343e0b
Nishanth KD <kd@coloredcow.in>
2016-04-22 15:28:47 +0530

    Email for Password Reset.

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,8 @@
-<p>Click here to reset your password: {{ url('password/reset/'.$token) }}</p>
+<p>Ocuhub User</p>
+<br>
+<p>We received a request from you to reset your account password.</p>
+<p>Click here to reset your password: {{ url('password/reset/'.$token) }} .</p>
+<p>The link would be active for one hour.</p>
+<br>
+<p>Support Staff</p>
+<p>Ocuhub</p>
