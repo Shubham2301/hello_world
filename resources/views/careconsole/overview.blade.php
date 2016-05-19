381719acb532cd4de32fe4bfd79f7bfc5c596293
Nishanth KD <kd@coloredcow.in>
2016-05-19 12:44:09 +0530

    #566 - CareConsole Overview Screen Responsiveness

--- resources/views/careconsole/overview.blade.php
+++ resources/views/careconsole/overview.blade.php
@@ -1,6 +1,6 @@
 <div class="overview row before_drilldown">
     @foreach($overview['stages'] as $stage)
-    <div class="overview_section col-xl-2 col-lg-3 col-md-4 col-xs-6">
+    <div class="overview_section col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-12">
         <div class="info_box" id="{{ $stage['name'] }}" data-id="{{ $stage['id'] }}" data-name="{{ $stage['display_name'] }}">
             <div class="top">
                 @if(isset($stage['kpis']))
