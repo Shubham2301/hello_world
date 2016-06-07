533f8177b6fd23af4c1d451a1f4ee7a44b3a28cf
bohraji <tusharbohra01@gmail.com>
2016-06-07 12:09:19 +0530

    email validation case sensitivity removed

--- resources/assets/js/patient.js
+++ resources/assets/js/patient.js
@@ -580,7 +580,7 @@ function showModalConfirmDialog(msg, handler) {
 
 function checkForm() {
     var fields = $('.panel-body').find('.add_patient_input');
-    var patt = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/g;
+    var patt = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
     fields.each(function (field) {
         if ($(this).prop('required')) {
             if ($(this).val() == "") {
