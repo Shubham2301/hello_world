490f6187065399cc786138d99be9403ecfffeed2
Nishanth KD <kd@coloredcow.in>
2016-06-08 15:31:59 +0530

    #515 - error logging on client side for dr. flemings ses account.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,8 @@ Route::post('password/email', 'Auth\PasswordController@postEmail');
 // Password reset routes...
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
+Route::post('errors/directmail/', 'DirectMail\DirectMailController@logClientError');
+
 
 Route::group(['middleware' => 'auth'], function () {
 
