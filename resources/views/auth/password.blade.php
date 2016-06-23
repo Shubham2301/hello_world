3b20fb6396a488c7f2ac58f680e420a86fa8460d
pankaja455 <pankaja455@gmail.com>
2016-06-23 15:40:21 +0530

    UX improvements in reset password functionality. #510

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,9 +1,6 @@
-@extends('layouts.master')
-@section('title', 'Ocuhub - Reset Password')
-@section('content')
+@extends('layouts.master') @section('title', 'Ocuhub - Reset Password') @section('content')
 <form method="POST" action="/password/email">
-    {!! csrf_field() !!}
-    @if (count($errors) > 0)
+    {!! csrf_field() !!} @if (count($errors) > 0)
     <div class="row" id="flash-message">
         @foreach ($errors->all() as $error)
         <div class="col-sm-12 left-padding top-margin">
@@ -11,10 +8,13 @@
         </div>
         @endforeach
     </div>
-    @endif
+    @endif @if(session()->has('success'))
+	<p class="arial side_padding">Sent!</p>
+	<p class="side_padding top_margin">The link expires in an hour.</p>
+    @else
     <div class="row content-row-margin">
         <div class="col-sm-12">
-            <p class="white-text">Please sign in </p>
+            <p class="white-text">Reset password </p>
         </div>
     </div>
     <div class="row content-row-margin">
@@ -27,5 +27,8 @@
             <button type="submit" class="button" type="submit">Send Password Reset Link</button>
         </div>
     </div>
-</form>
+	{!! Form::close() !!}
+	@endif
+	<p class="side_padding" style="padding-top: 5em;">Go back to <a title="" style="font-weight: bold; text-decoration: underline;" href="/auth/logout" data-original-title="">Sign in</a></p>
+
 @endsection
