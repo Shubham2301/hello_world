fb5eef8e2e86d4e5b5f06d777d2624a688304a16
Nishanth KD <kd@coloredcow.in>
2016-07-04 16:15:39 +0530

    Jobs for Confirm Appointment

---
+++ app/Jobs/PatientEngagement/ConfirmAppointmentPatientSMS.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace myocuhub\Jobs\PatientEngagement;
+
+use myocuhub\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ConfirmAppointmentPatientSMS extends PatientEngagement implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
