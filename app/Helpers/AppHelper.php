9672a091efddcb7f7de4918c2ab9890f82ca4a42
Nishanth KD <kd@coloredcow.in>
2016-07-25 15:27:48 +0530

    Conflict resolution on ifExists

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -5,11 +5,7 @@ namespace myocuhub\Helpers;
 trait AppHelper{
 
 	public static function ifExists($key, $array){
-<<<<<<< HEAD
-		return isset($key, $array) ? $array[$key] : null; 
-=======
 		return isset($array[$key]) ? $array[$key] : null;
->>>>>>> 97fcd2e... Helpers on ifExists
 	}
 
 	public static function invertConfig($expression){
