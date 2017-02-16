28b8ca0afd1d030908f2939881499351f7463e83
Tushar Singh Bohra <tushar@coloredcow.in>
2017-02-16 16:50:12 +0530

    #1333 additional check for missing practice (#1334)

--- resources/assets/js/users.js
+++ resources/assets/js/users.js
@@ -406,6 +406,16 @@ function checkForm() {
         flag = 1;
         return false;
     }
+    if ($('.add_user_input#user_level').val() > 2 && $('.add_user_input#user_practice').val() == null && window.location.pathname === '/administration/users/create') {
+        $('#user_practice').show();
+        $('#user_practice').prop('required', true);
+        $('#user_practice').attr('required', true);
+        $('.user_network').prop('checked', false);
+        $('#collapse1').parents('.panel-default').find('.popover_text').attr('data-content', 'Please select a network and practice for the user');
+        $('#collapse1').parents('.panel-default').find('.popover_text').popover("show");
+        flag = 1;
+        return false;
+    }
 
     if (flag == 0)
         return true;
