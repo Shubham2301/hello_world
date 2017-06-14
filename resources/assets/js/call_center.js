4ab856d2338417e12c369a30d76a595ef740670c
Tushar Singh Bohra <tushar@coloredcow.in>
2017-06-14 18:04:12 +0530

    #1410 super admin report date time picker updates (#1412)

--- resources/assets/js/call_center.js
+++ resources/assets/js/call_center.js
@@ -32,23 +32,6 @@ $(document).ready(function() {
     });
     var old_start_date = $('#start_date').val();
     var old_end_date = $('#end_date').val();
-    
-    var cur_date = new Date();
-    var set_start_date = new Date(cur_date.getTime());
-    $('#start_date').datetimepicker({
-        defaultDate: set_start_date.setDate(cur_date.getDate() - 31),
-        format: 'MM/DD/YYYY',
-        maxDate: cur_date,
-    });
-    set_start_date = new Date(cur_date.getTime());
-    $('#end_date').datetimepicker({
-        defaultDate: cur_date,
-        format: 'MM/DD/YYYY',
-        maxDate: cur_date,
-        minDate: set_start_date.setDate(cur_date.getDate() - 30),
-    });
-    var old_start_date = $('#start_date').val();
-    var old_end_date = $('#end_date').val();
 
     $('#start_date').datetimepicker().on('dp.hide', function(ev) {
         var start_date = $('#start_date').val();
